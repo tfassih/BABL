20ed82cb7a1c190c8f1b0218faa7bfb29de2de35
THOMAS FASSIH <thomasfassih@gmail.com>
2022-12-09 02:31:41 -0600

    initial commit

---
+++ src/test/java/com/tfassih/babl/BablApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfassih.babl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BablApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
